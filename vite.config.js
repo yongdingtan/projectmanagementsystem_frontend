59a014095796a976984a873f31d316b2a1083216
yongdingtan <yongdingimbatan@hotmail.com>
2025-03-08 20:46:33 +0800

    render port update

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
@@ -6,8 +7,11 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    port: process.env.PORT || 4173, // Use Render's environment variable or fallback to 4173
+    host: '0.0.0.0', // Ensure it's accessible on all interfaces
   },
 })
