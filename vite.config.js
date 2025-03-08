d9b389ce7fe48b602ad90302ef5aa24bb99496e1
yongdingtan <yongdingimbatan@hotmail.com>
2025-03-08 20:50:27 +0800

    allow frontend host in vite server

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,15 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
     port: process.env.PORT || 4173, // Use Render's environment variable or fallback to 4173
     host: '0.0.0.0', // Ensure it's accessible on all interfaces
+  },
+  preview: {
+    allowedHosts: ['projectmanagementsystem-frontend.onrender.com'], // Allow the frontend host
   },
 })
