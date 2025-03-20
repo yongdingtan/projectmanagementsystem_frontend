0e4014ebe5bd8b9f2794741fcf39803077fecba8
yongdingtan <yongdingimbatan@hotmail.com>
2025-03-20 16:49:42 +0800

    production testing

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8080"; // Localhost
-//export const API_BASE_URL = "https://projectmanagementsystem-latest.onrender.com"; // Render
+//export const API_BASE_URL = "http://localhost:8080"; // Localhost
+export const API_BASE_URL = "https://projectmanagementsystem-latest.onrender.com"; // Render
 
 const api = axios.create({
   baseURL: API_BASE_URL,
