86f2cdf6e348b0c43b7b9447af382ebcd3e3a6ff
yongdingtan <yongdingimbatan@hotmail.com>
2025-06-02 16:52:54 +0800

    added env

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-//export const API_BASE_URL = "http://localhost:8080"; // Localhost
-export const API_BASE_URL = "https://projectmanagementsystem-latest.onrender.com"; // Render
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
